cc33d0a820f3fe004d19c95cecc717dcd8283c20
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-06-15 17:00:56 -0400

    User created in database

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -8,37 +8,40 @@ import { Generators } from '@helpers/generators/generators';
 import { IAuthDocument } from '@auth/interfaces/IAuthDocument.interface';
 import HTTP_STATUS from 'http-status-codes';
 import { SignUpUtility } from './utilities/signup.utility';
-
-
-
+import { IUserDocument } from '@user/interfaces/IUserDocument.interface';
+import { userService } from '@services/db/user.service';
 
 
 export class SignUp extends SignUpUtility {
-  @joiValidation(signupSchema)
-  public async create(req: Request, res: Response): Promise<void> {
-    const { username, email, password, avatarColor } = req.body;
-    const checkIfUserExist = await authService.getUserByUsernameOrEmail(username, email);
-    if (checkIfUserExist) {
-      throw new BadRequestError('Invalid credentials for this user');
+    @joiValidation(signupSchema)
+    public async create(req: Request, res: Response): Promise<void> {
+        const { username, email, password, avatarColor } = req.body;
+        const checkIfUserExist = await authService.getUserByUsernameOrEmail(username, email);
+        if (checkIfUserExist) {
+            throw new BadRequestError('Invalid credentials for this user');
+        }
+
+        const authObjectId: ObjectId = new ObjectId();
+        const userObjectId: ObjectId = new ObjectId();
+
+        const passwordHash = await Generators.hash(password);
+        const authData: IAuthDocument = SignUp.prototype.signUpData({
+            _id: authObjectId,
+            username,
+            email,
+            password: passwordHash,
+            avatarColor
+        });
+
+
+        const userJwt: string = SignUp.prototype.signToken(authData, userObjectId);
+        req.session = { jwt: userJwt };
+
+        const user: IUserDocument = SignUp.prototype.userData(authData, userObjectId);
+        await userService.addUserData(user);
+
+        res
+            .status(HTTP_STATUS.CREATED)
+            .json({ message: 'User created successfully', user: user, token: userJwt });
     }
-
-    const authObjectId: ObjectId = new ObjectId();
-    const userObjectId: ObjectId = new ObjectId();
-
-    const passwordHash = await Generators.hash(password);
-    const authData: IAuthDocument = SignUp.prototype.signUpData({
-      _id: authObjectId,
-      username,
-      email,
-      password: passwordHash,
-      avatarColor
-    });
-
-    const userJwt: string = SignUp.prototype.signToken(authData, userObjectId);
-    req.session = { jwt: userJwt };
-
-    res
-      .status(HTTP_STATUS.CREATED)
-      .json({ message: 'User created successfully', user:  userObjectId, token: userJwt });
-  }
 }
