6f26961cd4b809dcaf9bbbea2279cf0fd7eccf6c
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2023-06-19 23:24:37 -0400

    task schema, documnet

--- src/features/task/models/task.schema.ts
+++ src/features/task/models/task.schema.ts
@@ -1,4 +1,15 @@
-//Se importa las clases Schema, model y Document del paquete mongoose.
-import mongoose, { Schema, model, Document } from 'mongoose';
+import { ITaskDocument } from '@task/interfaces/ITaskDocument';
 
+import mongoose, {Model, model, Schema } from 'mongoose';
 
+const taskSchema: Schema = new Schema({
+    _id: { type: String, required: true },
+    authId: { type: mongoose.Schema.Types.ObjectId, ref:'Auth'},
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const TaskModel: Model<ITaskDocument> = model<ITaskDocument>('Task', taskSchema);
+export { TaskModel };
