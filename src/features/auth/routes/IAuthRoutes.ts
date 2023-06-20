fff6f9f8883de36ac55915421de731b1fde40913
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2023-06-20 16:10:33 -0400

    ItaskRoutes routesd

--- src/features/auth/routes/IAuthRoutes.ts
+++ src/features/auth/routes/IAuthRoutes.ts
@@ -3,27 +3,24 @@ import { SignIn } from '@auth/controllers/signin';
 import { SignUp } from '@auth/controllers/signup';
 import { SignOut } from '@auth/controllers/signout';
 
-
 class AuthRoutes {
-    private router: Router;
+  private router: Router;
 
-    constructor() {
-        this.router = express.Router();
-    }
+  constructor() {
+    this.router = express.Router();
+  }
 
-    public routes(): Router {
+  public routes(): Router {
+    this.router.post('/signup', SignUp.prototype.create);
+    this.router.post('/signin', SignIn.prototype.read);
+    return this.router;
+  }
 
-        this.router.post('/signup', SignUp.prototype.create);
-        this.router.post('/signin', SignIn.prototype.read);
+  public signoutRoute(): Router {
+    this.router.get('/signout', SignOut.prototype.update);
 
-        return this.router;
-    }
-
-    public signoutRoute(): Router {
-        this.router.get('/signout', SignOut.prototype.update);
-
-        return this.router;
-    }
+    return this.router;
+  }
 }
 
 export const authRoutes: AuthRoutes = new AuthRoutes();
--- src/features/task/schemes/taskValidator.ts
+++ src/features/task/schemes/taskValidator.ts
@@ -1,8 +1,18 @@
 import Joi from 'joi';
 
-const taskValidator = Joi.object({
-  title: Joi.string().required(),
-  description: Joi.string().required(),
+const taskValidator = Joi.object().keys({
+  title: Joi.string().required().min(4).max(8).messages({
+    'string.base': 'Title Task must be a string',
+    'string.min': 'Invalid Title Task',
+    'string.max': 'Invalid Title Task',
+    'string.empty': 'Title Task must not be empty'
+  }),
+  description: Joi.string().required().min(7).max(50).messages({
+    'string.base': 'Description Task must be a string',
+    'string.min': 'Invalid Description task',
+    'string.max': 'Invalid Description task',
+    'string.empty': 'Description Task must not be empty'
+  }),
   completed: Joi.boolean().default(false),
 });
 
