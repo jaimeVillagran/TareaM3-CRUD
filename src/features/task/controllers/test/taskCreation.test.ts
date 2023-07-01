1d940892a7562067c1bbeee54a9c3ac3a44191a0
GabrielRecabarren <gabriel.recabarren@gmail.com>
2023-07-01 00:55:33 -0400

    detail

--- src/features/task/controllers/test/taskCreation.test.ts
+++ src/features/task/controllers/test/taskCreation.test.ts
@@ -1,56 +1,62 @@
-import { taskCreatedMock, taskDataMock, taskMockRequest } from "@root/mocks/task.mock";
-import { TaskService } from "@services/db/task.service";
-import { PASSWORD, USERNAME, authMockRequest, authMockResponse, authUserPayload, JWT, userMock, IJWT } from "@root/mocks/auth.mock";
-import { Request, Response } from "express";
+import { taskCreatedMock, taskDataMock, taskMockRequest } from '@root/mocks/task.mock';
+import { TaskService } from '@services/db/task.service';
+import {
+  PASSWORD,
+  USERNAME,
+  authMockRequest,
+  authMockResponse,
+  authUserPayload,
+  JWT,
+  userMock,
+  IJWT
+} from '@root/mocks/auth.mock';
+import { Request, Response } from 'express';
 import { TaskController } from '../taskController';
-import { CustomError } from "@helpers/errors/customError";
-import { authService } from "@services/db/auth.service";
-
+import { authService } from '@services/db/auth.service';
 
 jest.mock('@services/db/auth.service');
 jest.mock('@services/db/task.service');
 jest.useFakeTimers();
 
 describe('Task Creation', () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.clearAllTimers();
+  });
 
-    beforeEach(() => {
-        jest.resetAllMocks();
+  it('Should create a new task', async () => {
+    // GIVEN STEP
+    const req: Request = taskMockRequest(
+      { jwt: JWT },
+      {
+        title: 'Título de la tarea',
+        description: 'Descripción de la tarea',
+        completed: false
+      },
+      authUserPayload
+    ) as Request;
 
+    const res: Response = authMockResponse();
+
+    jest.spyOn(authService, 'getAuthUserByUsername').mockResolvedValue(userMock);
+    const taskCreation = jest.spyOn(TaskService.prototype, 'createTask');
+    console.log('Task Creation: ', taskCreation);
+
+    // WHEN
+    await TaskController.prototype.createTask(req, res);
+
+    // THEN
+    expect(req.session?.jwt as IJWT).toBeDefined();
+    expect(res.status).toHaveBeenCalledWith(201);
+    console.log('El undefined: ', taskCreation.mock.calls[0][0], 'Revisando el orden en el arreglo.');
+
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'Task created',
+      task: taskCreation.mock.calls[0][0]
     });
-
-    afterEach(() => {
-        jest.clearAllMocks();
-        jest.clearAllTimers();
-    });
-
-    it('Should create a new task', async () => {
-        // GIVEN STEP
-        const req: Request = taskMockRequest({ jwt: JWT }, {
-            title: 'Título de la tarea',
-            description: 'Descripción de la tarea',
-            completed: false
-        }, authUserPayload) as Request;
-
-        const res: Response = authMockResponse();
-
-        jest.spyOn(authService, 'getAuthUserByUsername').mockResolvedValue(userMock);
-        const taskCreation = jest.spyOn(TaskService.prototype, 'createTask');
-        console.log("Task Creation: ", taskCreation)
-
-
-        // WHEN
-        await TaskController.prototype.createTask(req, res);
-
-        // THEN
-        expect(req.session?.jwt as IJWT).toBeDefined();
-        expect(res.status).toHaveBeenCalledWith(201);
-        console.log("El undefined: ", taskCreation.mock.calls[0][0], 'Revisando el orden en el arreglo.');
-
-
-
-        expect(res.json).toHaveBeenCalledWith({
-            message: 'Task created', task: taskCreation.mock.calls[0][0]
-        });
-    });
+  });
 });
